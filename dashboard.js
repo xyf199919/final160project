a4c6f8376f6adf1a98bc541e9fcd8d8eb936a95e
“Elaine <wangelaine@berkeley.edu>
2019-03-03 01:56:13 -0800

    add event button

---
+++ dashboard.js
@@ -0,0 +1,41 @@
+function addevent(){
+
+  var ref = firebase.database().ref();
+  var postsRef = ref.child("12345").child("xyf199919").child(events);
+  var eventname = document.getElementById("eventname").value;
+  var eventloc = document.getElementById("eventloc").value;
+  var eventdate = document.getElementById("eventdate").value;
+  var eventstart = document.getElementById("eventstart").value;
+  var eventend = document.getElementById("eventend").value;
+  var newPostRef = postsRef.push({
+    classname: eventname,
+    date: eventdate,
+    starttime: eventstart,
+    endtime: eventend,
+    location: eventloc
+  });
+  var eventid = newPostRef.key;
+
+  ref.child(groupname).child(username).child(events).set(eventid);
+  ref.child(groupname).child(username).child(events).child(eventid).set(eventname);
+  ref.child(groupname).child(username).child(events).child(eventid).set(eventdate);
+  ref.child(groupname).child(username).child(events).child(eventid).set(eventstart);
+  ref.child(groupname).child(username).child(events).child(eventid).set(eventloc);
+  ref.child(groupname).child(username).child(events).child(eventid).set(eventstart);
+
+  // ref.once("value")
+  // .then(function(snapshot) {
+  //
+  //   //
+  //   var path = joingroupcode + "/" + newusername;
+  //   window.alert(path);
+  //   var c = snapshot.child(path).exists();
+  //   if (c) {
+  //       document.location.href = "dashboard.html";
+  //
+  //   }
+  //
+  //   // ...
+  // });
+
+}
